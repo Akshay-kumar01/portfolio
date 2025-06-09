38c64910ef7654d398bbe91bc97df15306edc2b4
Akshay Kumar Singh <75743797+Akshay-kumar01@users.noreply.github.com>
2025-06-09 19:25:32 +0530

    Updated WorkExperience.js

--- src/WorkExperience.js
+++ src/WorkExperience.js
@@ -32,7 +32,7 @@ function WorkExperience() {
         "Formulated captivating, engaging, impactful graphics for the organisation's social media posts.",
         "Supervised and directed the efforts of fellow graphic designers as a team lead of 20 members."
       ],
-      certificateLink: "master/public/Akshay Kumar SIngh Pinkishe.png", // Certificate link for Pinkishe Foundation
+      certificateLink: "master/public/Akshay_Kumar_SIngh_Pinkishe.png", // Certificate link for Pinkishe Foundation
       lorCertificateLink: "/LOR Akshay Kumar Singh.png" // Link to the LOR PDF
     }
   ];
